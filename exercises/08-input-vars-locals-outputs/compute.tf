22db796bd5edd5e8ccb6a916ed51adea0ce8a9a9
samartinezcuello <sa.martinez.cuello@gmail.com>
2026-06-24 18:35:32 +0200

    exercise16

--- exercises/08-input-vars-locals-outputs/compute.tf
+++ exercises/08-input-vars-locals-outputs/compute.tf
@@ -13,13 +13,28 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+# resource "aws_instance" "compute" {
+#   ami           = data.aws_ami.ubuntu.id
+#   instance_type = var.ec2_instance_type
+
+#   root_block_device {
+#     delete_on_termination = true
+#     volume_size           = var.ec2_volume_size
+#     volume_type           = var.ec2_volume_type
+#   }
+# }
+
 resource "aws_instance" "compute" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.ec2_instance_type
 
   root_block_device {
     delete_on_termination = true
-    volume_size           = var.ec2_volume_size
-    volume_type           = var.ec2_volume_type
+    volume_size           = var.ec2_volume_config.size
+    volume_type           = var.ec2_volume_config.type
   }
+
+  tags = merge(var.additional_tags,{
+    ManagedBy = "Terraform"
+  })
 }
--- exercises/08-input-vars-locals-outputs/variables.tf
+++ exercises/08-input-vars-locals-outputs/variables.tf
@@ -27,3 +27,21 @@ variable "ec2_volume_size" {
   description = "EC2 Volume Size"
   default     = 10
 }
+
+variable "ec2_volume_config" {
+  type = object({
+    type = string
+    size = number
+  })
+
+  description = "The size and type of the root block volume for EC2 instances."
+  default = {
+    size = 10
+    type = "gp3"
+  }
+}
+
+variable "additional_tags" {
+  type    = map(string)
+  default = {}
+}
